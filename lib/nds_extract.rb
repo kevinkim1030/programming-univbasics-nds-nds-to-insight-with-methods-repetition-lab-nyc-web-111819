3f1b7851d89563c58946e23863da046b795f903a
Kevin Kim <kevin.kim1030@gmail.com>
2019-11-15 01:32:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -55,7 +55,7 @@ end
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  total
+
 
 
 
